1d7feb581f73835850ae9427c0fec6aad2fb6a97
lifeprofessor <lifeprofessor>
2022-10-26 08:50:40 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 "use strict"
 
-console.log('hello');
+// 컨트롤러의 id에 해당되는 태그 리퀘스트 : document.querySelector
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#pw"),
+    loginBtn = document.querySelector("#login");
+
+// 버튼이 클릭됐을 때 이벤트 login 함수 실행
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        pw : psword.value,
+    };
+    console.log(req);
+}
