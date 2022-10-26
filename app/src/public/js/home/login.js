071ec3c038b865f96c782bd46bc70ffd46fb9846
lifeprofessor <lifeprofessor>
2022-10-26 12:20:57 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,7 +2,7 @@
 
 // 컨트롤러의 id에 해당되는 태그 리퀘스트 : document.querySelector
 const id = document.querySelector("#id"),
-    psword = document.querySelector("#pw"),
+    pw = document.querySelector("#pw"),
     loginBtn = document.querySelector("#login");
 
 // 버튼이 클릭됐을 때 이벤트 login 함수 실행
@@ -11,7 +11,7 @@ loginBtn.addEventListener("click", login);
 function login(){
     const req = {
         id : id.value,
-        pw : psword.value,
+        pw : pw.value,
     };
      console.log(req);
      console.log(JSON.stringify(req));
@@ -23,6 +23,8 @@ function login(){
             "Content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
+    })
+    .then((res)=>res.json())
+    .then((res)=>console.log(res));
 
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { unsubscribe } = require(".");
+
 const output = {
     home: (req, res) =>{
         res.render("home/index");
@@ -10,10 +12,32 @@ const output = {
     },    
 };
 
+const users = {
+    id: ["warmars87", "warmars"],
+    pw: ["1234","1234"],
+};
+
 const process = {
     login : (req, res) =>{
-        console.log('서버 접근');    
-        console.log(req.body); 
+        console.log('---서버에 전송된 내용---');    
+        const id = req.body.id,
+            pw=req.body.pw;
+
+        console.log(id, pw);
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.pw[idx]=== pw){
+                 return res.json({
+                    success: true,
+                 })
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패했습니다.",
+        });
     },
 };
 
