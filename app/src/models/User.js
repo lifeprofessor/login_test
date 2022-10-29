4aa19916fd13c359fd5f5fa55113e2ef608dcb28
lifeprofessor <lifeprofessor>
2022-10-29 09:49:36 +0900

    RDS로 DB연결해 회원가입

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -13,16 +13,21 @@ class User {
 
         // await은 promise의 pending(다 못받음)을 처리해줌
             // await은 async 함수에서만 사용 가능, 비동기 처리
-        const {id, pw} = await UserStorage.getUserInfo(client.id); 
+        try{
+            const {id, pw} = await UserStorage.getUserInfo(client.id); 
 
-        // id 정보 판별
-        if(id){
-            if(id === client.id && pw === client.pw){
-                return { success : true}
+            // id 정보 판별
+            if(id){
+                if(id === client.id && pw === client.pw){
+                    return { success : true}
+                }
+                return {success : false, msg: "비번이 달라요"};            
             }
-            return {success : false, msg: "비번이 달라요"};            
+            return {success : false, msg: "존재하지 않는 아이디입니다."};        
+        }catch(err){
+            return {success: false, msg: err};
         }
-        return {success : false, msg: "존재하지 않는 아이디입니다."};        
+        
 
     }
 
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,49 +3,30 @@
 const db = require("../config/db");
 
 class UserStorage{
-    static #getUserInfo(data, id){
-        const users = JSON.parse(data);
-            const idx = users.id.indexOf(id);   // users의 id값에 해당하는 인덱스
-            const userKeys = Object.keys(users);// users의 키값 받음
-            
-            const userInfo = userKeys.reduce((newUser, info)=>{ // idx에 해당하는 id의 데이터 받음
-                newUser[info] = users[info][idx];
-                return newUser;
-            }, {});
-            return userInfo;
-    }
-
-    static #getUsers(data, isAll, fields) {
-
-        const users = JSON.parse(data); // 버퍼 데이터를 파싱해 JSON으로 변환
-        if(isAll) return users;
-
-        const newUsers = fields.reduce((newUsers, field)=> {
-            if(users.hasOwnProperty(field)){
-                // hasOwnPorperty - 키 값이 있는지 확인
-                newUsers[field] = users[field];
-            }
-            return newUsers;    // 누적된 값 리턴
-        }, {});
-        return newUsers;
-    }
-
-    // 변수가 n개인 경우 사용법
-    static getUsers(isAll, ...fields){
-        
-    }
-
+    
+    // 유저 정보 DB검색 후 Promise로 리턴
     static getUserInfo(id){
         // Promise 안에 구문이 성공하면 resolve실행, 실패하면 reject 실행
         return new Promise((resolve, reject)=>{
-            db.query("select * from users where id = ?", [id], (err, data)=>{
-                if(err) reject(err);
+            const query = "select * from users where id = ?;";
+            db.query(query, [id], (err, data)=>{
+                if(err) reject(`${err}`);
                 resolve(data[0]);   // 배열로 전달되므로 배열로 1개의 객체만 보냄
             });
         });
     }
 
     static async save(userInfo){
+        return new Promise((resolve, reject)=>{
+            const query = "insert into users(id, pw, name) values(?, ?, ?);";
+            db.query(
+                query, 
+                [userInfo.id, userInfo.pw, userInfo.name], 
+                (err)=>{
+                if(err) reject(`${err}`);
+                resolve({success : true});   // 배열로 전달되므로 배열로 1개의 객체만 보냄
+            });
+        });
     }
 
 }
